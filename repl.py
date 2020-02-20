af4543ed5ae887fb57ac7f79645809ad85a27e9a
henry <tahafut@gmail.com>
2020-02-19 21:46:37 -0800

    initial commit

---
+++ repl.py
@@ -0,0 +1,37 @@
+import json
+import atexit
+
+try:
+    f = open('data.json')
+except FileNotFoundError:
+    data = {}
+    f = open('data.json', 'w')
+    json.dump(data, f)
+else:
+    data = json.load(f)
+finally:
+    f.close()
+
+def save():
+    with open('data.json', 'w') as f:
+        json.dump(data, f)
+
+def process(text):
+    command, option = text.strip().split()
+    if command == 'get':
+        key = option
+        return data[key]
+    if command == 'set':
+        key, value = option.split('=')
+        data[key] = value
+        return 'OK'
+
+atexit.register(save)
+
+while True:
+    try:
+        text = input('> ')
+    except KeyboardInterrupt:
+        break
+    else:
+        print(process(text))
